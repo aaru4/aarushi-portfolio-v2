4fd519011b3ff289648324c57d4c0acd2b16401f
Aarushi Ammavajjala <aaru7811@gmail.com>
2024-12-23 23:49:50 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
+import Head from "next/head";
 
 export default function Home() {
   return (
+    <>
+    <Head>
+      <title>Aarushi Ammavajjala</title>
+    </Head>
     <div className="grid grid-rows-[10px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <h1> aarushi ammavajjala </h1>
-        <h2> developer, studying computer science @ tbd </h2>
+        <h2> developer, studying computer science @ south forsyth high school </h2>
       </main>
     </div>
+    </>
   );
 }
