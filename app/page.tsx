978f9fa860890e0a2d2933e1b85a9c619772b819
Aarushi Ammavajjala <aaru7811@gmail.com>
2024-12-23 23:54:54 -0500

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     <div className="grid grid-rows-[10px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <h1> aarushi ammavajjala </h1>
-        <h2> developer, studying computer science @ south forsyth high school </h2>
+        <h2> developer, studying computer science @ tbd </h2>
       </main>
     </div>
     </>
