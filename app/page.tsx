ca381a3139386c5eff59bd231c8deb4de17fec2c
Aarushi Ammavajjala <aaru7811@gmail.com>
2024-12-26 18:54:22 -0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import Navbar from './components/header';
 import { ProjectBox } from './components/ProjectCard';
 
-// color palette: #fccf97, #faf2b9, #bddca7, #a7dccd
+// color palette: #faf2b9, #bddca7, #a7dccd, #ebc2ff
 
 export default function Home() {
   return (
@@ -14,7 +14,7 @@ export default function Home() {
           <h1 className="font-bold text-5xl px-2 py-2">Aarushi Ammavajjala</h1>
         </div>
             <h1 className="font-bold text-xl py-3"> Developer </h1>
-            <h1 className="text-l"> Studying Computer Science @ South Forsyth High School. </h1>
+            <h1 className="text-l"> Studying Computer Science @ South Forsyth High School </h1>
             <h1 className="text-l"> Contact me: aaru7811 [at] gmail [dot] com </h1>
             <a
               href="https://www.linkedin.com/in/aarushi-ammavajjala-207b49238/"
@@ -27,20 +27,20 @@ export default function Home() {
                   alt="LinkedIn Icon"
                   width={40}
                   height={40}
-                  className="rounded hover:bg-[#a7dccd] opacity-50 transition-opacity duration-200"
+                  className="rounded hover:bg-[#a7dccd] transition-opacity-80 duration-200"
                 />
             </a>
         </section>
         <section id="education">
-          <h1 className="font-bold text-xl py-3"> Education </h1>
+          <h1 className="text-2xl py-3"> Education </h1>
           <ProjectBox title="South Forsyth High School" org="2021-2025" color="#fccf97" />
         </section>
         <section id="experience">
         <div className="flex flex-wrap gap-8">
-          <ProjectBox title="Machine Learning Intern" org="Stanford AIMI Research Group" color="#fccf97" />
           <ProjectBox title="AI Researcher" org="Cornell University" color="#faf2b9" />
           <ProjectBox title="Machine Learning Intern" org="Stanford AIMI Research Group" color="#bddca7" />
           <ProjectBox title="AI Researcher" org="Cornell University" color="#a7dccd" />
+          <ProjectBox title="Machine Learning Intern" org="Stanford AIMI Research Group" color="#ebc2ff" />
         </div>
         </section>
       </main>
