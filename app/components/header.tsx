92310e45de98b4faf1a6dd241acb0b2148d8a08a
Aarushi Ammavajjala <aaru7811@gmail.com>
2024-12-26 13:14:42 -0500

    Update

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -3,23 +3,27 @@ import Link from 'next/link';
 const Navbar = () => {
   return (
     <nav>
-      <ul className="flex space-x-4">
-        <li className="font-bold text-xl p-2 rounded" style={{ backgroundColor: "#a7dccd" }}>
-          Aarushi Ammavajjala
-        </li>                                                 
-        <li>
-          <Link href="#home">Home</Link>
-        </li>
-        <li>
-          <Link href="#education">Education & Skills</Link>
-        </li>
-        <li>
-          <Link href="#experience">Experience</Link>
-        </li>
-        <li>
-          <Link href="#projects">Projects</Link>
-        </li>
-      </ul>
+      <div className="flex flex-wrap gap-8 items-left">
+        <ul className="flex space-x-4">
+          <li className="font-bold text-xl p-2 rounded" style={{ backgroundColor: "#a7dccd" }}>
+            Aarushi Ammavajjala
+          </li>   
+        </ul>
+        <ul className="flex space-x-4 ml-auto">  {}
+          <li>
+            <Link href="#home">Home</Link>
+          </li>
+          <li>
+            <Link href="#education">Education & Skills</Link>
+          </li>
+          <li>
+            <Link href="#experience">Experience</Link>
+          </li>
+          <li>
+            <Link href="#projects">Projects</Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
