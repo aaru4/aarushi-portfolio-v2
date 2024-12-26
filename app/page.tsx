410f90b2f517659a85780939bb8b6c88025c57f8
Aarushi Ammavajjala <aaru7811@gmail.com>
2024-12-26 10:07:42 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import Header from './components/header';
 export default function Home() {
   return (
     <>
-    <div className="grid grid-rows-[10px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[auto_1fr]] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <Header />
-        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <main className="flex flex-col gap-8 row-start-2 items-left sm:items-start">
         <h1> aarushi ammavajjala </h1>
         <h2> developer, studying computer science @ tbd </h2>
       </main>
