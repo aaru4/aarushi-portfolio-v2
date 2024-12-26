ea89f3374796daa5ecaa42f32b09cbaa0382a905
Aarushi Ammavajjala <aaru7811@gmail.com>
2024-12-26 11:21:07 -0500

    Update

--- app/components/NavBar/Nav.tsx
+++ app/components/NavBar/Nav.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import styles from './Nav.module.css';
-import * as data from './links.json';
-const linksString = JSON.stringify(data);
-const links = JSON.parse(linksString).links;
+import { links } from './links.json';
 
 type Link = {
     label: string;
