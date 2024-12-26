7ae34ed0707f41ae67062925836c829a386ba74b
Aarushi Ammavajjala <aaru7811@gmail.com>
2024-12-26 11:26:21 -0500

    Update

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,12 +1,21 @@
-export default function Header() {
-    return (
-        <div>
-            <ul>
-                <li>Home</li>
-                <li>Education & Skills</li>
-                <li>Experience</li>
-                <li>Projects</li>
-            </ul>
-        </div>
-    )
-}
+import Link from 'next/link';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li>
+          <Link href="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import Nav from './components/NavBar/Nav';
+import Navbar from './components/header';
 import { ProjectBox } from './components/ProjectCard';
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[auto_1fr]] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Nav />
+      <Navbar />
         <main className="flex flex-col gap-8 row-start-2 items-left sm:items-start">
         <h1> aarushi ammavajjala </h1>
         <h2> developer, studying computer science @ tbd </h2>
