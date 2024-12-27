981134c9dbf34f484b20a66033fcb410e68e96c7
Aarushi Ammavajjala <aaru7811@gmail.com>
2024-12-26 20:35:29 -0500

    U'

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -8,7 +8,7 @@ interface ProjectCardProps {
   
   export const ProjectBox: React.FC<ProjectCardProps> = ({ title, org, date, desc, color }) => {
     return (
-      <div style={{...cardStyles.container, backgroundColor: color }}>
+      <div style={{...cardStyles.container, backgroundColor: color, ...cardHoverStyles.container}}>
         <h1 style={cardStyles.title}>{title}</h1>
         <p style={cardStyles.org}>{org}</p>
         <p style={cardStyles.date}>{date}</p>
