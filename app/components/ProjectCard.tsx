3c4450521c92a4cfda4e2ee3ec47e1f2716c482b
Aarushi Ammavajjala <aaru7811@gmail.com>
2024-12-26 20:59:49 -0500

    Update

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -6,10 +6,10 @@ interface ProjectCardProps {
     color: string;
   }
   
-  function hexToRgba(hex, opacity) {
-    let r = parseInt(hex.slice(1, 3), 16);
-    let g = parseInt(hex.slice(3, 5), 16);
-    let b = parseInt(hex.slice(5, 7), 16);
+  function hexToRgba(hex: string, opacity: number) {
+    const r = parseInt(hex.slice(1, 3), 16);
+    const g = parseInt(hex.slice(3, 5), 16);
+    const b = parseInt(hex.slice(5, 7), 16);
     return `rgba(${r}, ${g}, ${b}, ${opacity})`;
   }
 
