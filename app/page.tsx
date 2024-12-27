38f84a7dc6a5974e6a7e6258d6d67c153d89ac6e
Aarushi Ammavajjala <aaru7811@gmail.com>
2024-12-27 02:05:23 -0500

    :u:

--- app/page.tsx
+++ app/page.tsx
@@ -34,8 +34,8 @@ export default function Home() {
                 />
             </a>
             </section>
+            <h1 className="font-bold text-2xl py-3"> Education </h1>
             <section id="experience" className="flex flex-col gap-6 sm:flex-row sm:gap-8">
-          <h1 className="font-bold text-2xl py-3"> Education </h1>
   <div className="w-full sm:w-1/3">
     <UCProjectBox
       title="South Forsyth High School"
