2391e00f5d2d1bba20daf6894ce51e081fabc486
Aarushi Ammavajjala <aaru7811@gmail.com>
2024-12-26 20:33:06 -0500

    u

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import { SkillBox } from './components/SkillCard';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[auto_1fr]] items-left justify-items-left min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[auto_1fr] items-left justify-items-left min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <Navbar />
         <main className="flex flex-col gap-8 row-start-2 items-left sm:items-start">
         <section id="home" className="mt-0">
