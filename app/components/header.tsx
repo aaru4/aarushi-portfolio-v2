ef774104bca53cf07916f863734e2ef4606d4740
Aarushi Ammavajjala <aaru7811@gmail.com>
2025-02-05 18:28:58 -0500

    Update header.tsx

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -5,16 +5,16 @@ const Navbar = () => {
     <nav w-full>
       <div className="flex flex-wrap gap-8 pt-0 pb-0 items-left">
         <ul className="flex space-x-4 ml-auto">  { }
-          <li className="text-lg p-2 hover:bg-[#fbceb1] hover:text-black">
+          <li className="text-xl p-2 hover:bg-[#fbceb1] hover:text-black">
             <Link href="#home">Home</Link>
           </li>
-          <li className="text-lg p-2 hover:bg-[#b1ddc9] hover:text-black">
+          <li className="text-xl p-2 hover:bg-[#b1ddc9] hover:text-black">
             <Link href="#education">Education </Link>
           </li>
-          <li className="text-lg p-2 hover:bg-[#6cd0d0] hover:text-black">
+          <li className="text-xl p-2 hover:bg-[#6cd0d0] hover:text-black">
             <Link href="#experience">Experience</Link>
           </li>
-          <li className="text-lg p-2 hover:bg-[#ffcbdb] hover:text-black">
+          <li className="text-xl p-2 hover:bg-[#ffcbdb] hover:text-black">
             <Link href="#projects">Projects</Link>
           </li>
         </ul>
