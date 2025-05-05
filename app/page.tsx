8aa17f3cb2610ad17c894b75e968b56712486826
Aarushi Ammavajjala <aaru7811@gmail.com>
2025-05-05 17:11:51 -0400

    u

--- app/page.tsx
+++ app/page.tsx
@@ -16,15 +16,27 @@ export default function Home() {
   <Navbar />
   <main className="flex flex-col gap-6 sm:gap-8 row-start-2 items-start">
     <section id="home" className="mt-0">
-      <div className="inline-block max-w-full px-2 py-2 rounded bg-gradient-to-r from-[#b1ddc9] to-[#6cd0d0]">
-        <h1 className="font-bold text-3xl sm:text-4xl md:text-5xl px-2 py-2">
-          Aarushi Ammavajjala.
-        </h1>
-      </div>
-      <h2 className="font-bold text-xl sm:text-2xl md:text-3xl py-3">Developer</h2>
+    <div className="flex flex-col md:flex-row h-screen">
+  {/* Left/Top Section */}
+  <div className="w-full md:w-1/2 flex items-center justify-center bg-gradient-to-r from-[#b1ddc9] to-[#6cd0d0]">
+    <h1 className="font-bold text-3xl sm:text-4xl md:text-5xl px-4 py-4">
+      Aarushi Ammavajjala.
+    </h1>
+    <h2 className="font-bold text-xl sm:text-2xl md:text-3xl py-3">Developer</h2>
       <p className="text-base sm:text-lg md:text-xl">
         Studying Computer Science @ South Forsyth High School
       </p>
+  </div>
+  <div className="w-full md:w-1/2 bg-white"></div>
+  <Image
+          src="/linkedin.svg"
+          alt="LinkedIn Icon"
+          width={50}
+          height={50}
+          className="rounded hover:bg-[#6cd0d0] transition-opacity-80 duration-200"
+        />
+</div>
+
       <p className="text-base sm:text-lg md:text-xl">
         Contact: aaru7811 [at] gmail [dot] com
       </p>
