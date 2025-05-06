39aa3f4d438356594ea1f50ebcb90949a37570be
Aarushi Ammavajjala <aaru7811@gmail.com>
2025-05-06 17:04:41 -0400

    u

--- app/expPage/expPage.tsx
+++ app/expPage/expPage.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'; // Import Link component from Next.js
 export default function About() {
   return (
     <div>
-      <h1>About Page</h1>
+      <h1>Exp Page</h1>
       <p>
         <Link href="/">Back to Homepage</Link>
       </p>
