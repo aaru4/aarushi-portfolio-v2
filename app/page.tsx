4b40f6f0e387cfcec8ec4a85f548425191375c11
Aarushi Ammavajjala <aaru7811@gmail.com>
2025-05-05 22:09:30 -0400

    u

--- app/page.tsx
+++ app/page.tsx
@@ -13,11 +13,11 @@ export default function Home() {
   <main className="flex flex-col gap-6 sm:gap-8 row-start-2 items-start p-0 m-0 w-screen grow">
   <div>
   <section id="home" className="m-0 p-0 w-full">
-    <div className="flex flex-col md:flex-row h-screen w-full">
-    <div className="w-full md:w-1/2 h-1/2 md:h-full flex items-center justify-center bg-gradient-to-r from-[#b1ddc9] to-[#6cd0d0] p-6 sm:p-8 md:p-10">
+    <div className="flex flex-col md:flex-row h-screen w-screen">
+    <div className="w-screen md:w-1/2 h-1/2 md:h-full flex items-center justify-center bg-gradient-to-r from-[#b1ddc9] to-[#6cd0d0] p-6 sm:p-8 md:p-10">
     <div className="text-center md:text-left px-6">
           <h1 className="font-bold text-3xl sm:text-4xl md:text-5xl py-2">
-            Aarushi Amavajjala.
+            Aarushi Ammavajjala.
           </h1>
           <h2 className="font-bold text-xl sm:text-2xl md:text-3xl py-2">
             Developer
@@ -73,7 +73,7 @@ export default function Home() {
       <div
         className="rounded-lg py-6 px-4 bg-gradient-to-br from-[#ffcbdb80] to-[#ffcbdb] shadow-md"
       >
-        <h2 className="text-lg sm:text-2xl md:text-3xl text-black tracking-wide uppercase">
+        <h2 className="text-lg sm:text-2xl md:text-3xl text-black tracking-wide">
           Education & Skills
         </h2>
       </div>
