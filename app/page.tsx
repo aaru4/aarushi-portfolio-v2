6d358e0e1cbf004372ecb5360e068693503e45ba
Aarushi Ammavajjala <aaru7811@gmail.com>
2025-05-05 22:06:43 -0400

    u

--- app/page.tsx
+++ app/page.tsx
@@ -10,14 +10,14 @@ export default function Home() {
   return (
 <div className="grid grid-rows-[auto_1fr] min-h-screen w-screen p-0 m-0">
   {/* <Navbar /> */}
-  <main className="flex flex-col gap-6 sm:gap-8 row-start-2 items-start p-0 m-0 w-full grow">
+  <main className="flex flex-col gap-6 sm:gap-8 row-start-2 items-start p-0 m-0 w-screen grow">
   <div>
   <section id="home" className="m-0 p-0 w-full">
     <div className="flex flex-col md:flex-row h-screen w-full">
     <div className="w-full md:w-1/2 h-1/2 md:h-full flex items-center justify-center bg-gradient-to-r from-[#b1ddc9] to-[#6cd0d0] p-6 sm:p-8 md:p-10">
     <div className="text-center md:text-left px-6">
           <h1 className="font-bold text-3xl sm:text-4xl md:text-5xl py-2">
-            Aarushi Ammavajjala.
+            Aarushi Amavajjala.
           </h1>
           <h2 className="font-bold text-xl sm:text-2xl md:text-3xl py-2">
             Developer
@@ -73,7 +73,7 @@ export default function Home() {
       <div
         className="rounded-lg py-6 px-4 bg-gradient-to-br from-[#ffcbdb80] to-[#ffcbdb] shadow-md"
       >
-        <h2 className="text-lg sm:text-2xl md:text-3xl font-semibold text-black tracking-wide uppercase">
+        <h2 className="text-lg sm:text-2xl md:text-3xl text-black tracking-wide uppercase">
           Education & Skills
         </h2>
       </div>
@@ -85,7 +85,7 @@ export default function Home() {
       <div
         className="rounded-lg py-6 px-4 bg-gradient-to-br from-[#fbceb180] to-[#fbceb1] shadow-md"
       >
-        <h2 className="text-lg sm:text-2xl md:text-3xl font-semibold text-black tracking-wide">
+        <h2 className="text-lg sm:text-2xl md:text-3xl text-black tracking-wide">
           Recent Experiences
         </h2>
       </div>
@@ -97,7 +97,7 @@ export default function Home() {
       <div
         className="rounded-lg py-6 px-4 bg-gradient-to-br from-[#b1ddc980] to-[#b1ddc9] shadow-md"
       >
-        <h2 className="text-lg sm:text-2xl md:text-3xl font-semibold text-black tracking-wide">
+        <h2 className="text-lg sm:text-2xl md:text-3xl text-black tracking-wide">
           Recent Projects
         </h2>
       </div>
