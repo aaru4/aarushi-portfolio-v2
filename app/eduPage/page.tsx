7264a93ec3607b2c6cda1408c2efda05ee4eccc9
Aarushi Ammavajjala <aaru7811@gmail.com>
2025-05-06 20:57:53 -0400

    u

---
+++ app/eduPage/page.tsx
@@ -0,0 +1,12 @@
+
+const eduPage = () => {
+    return (
+      <div>
+        <h1>Welcome to the edu Page!</h1>
+        <p>This is the content of the experimental page.</p>
+      </div>
+    );
+  };
+  
+  export default eduPage;
+  
---
+++ app/projPage/page.tsx
@@ -0,0 +1,12 @@
+
+const projPage = () => {
+    return (
+      <div>
+        <h1>Welcome to the proj  Page!</h1>
+        <p>This is the content of the experimental page.</p>
+      </div>
+    );
+  };
+  
+  export default projPage;
+  
