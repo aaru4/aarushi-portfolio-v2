e109162b0fc67bd9df29148e8ddeb6df226db178
aaru4 <98666654+aaru4@users.noreply.github.com>
2025-05-28 22:44:57 -0400

    Update page.tsx

--- app/projPage/page.tsx
+++ app/projPage/page.tsx
@@ -17,7 +17,20 @@ const projPage = () => {
 </div>    
 
 <div className="w-screen pb-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 place-items-center gap-6 pt-6 px-6">
-          <a
+            <a
+              href="https://github.com/Suchit-07/Lung-and-Colon-Detection-Model-Website/tree/main/public"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="inline-block mt-4"
+            >
+              <IndBox
+                title="LUMINA: LUng, colon, Multi-class Imaging via Noninvasive Analysis"
+                org="Low-cost, multi-class ultrasound analysis to identify different types of cancer (lung adenocarcinomas, squamous cell carcinomas, colon adenocarcinomas, etc.). Leverages ResNet18 architecture enhanced with a custom attention layer to improve feature focus and classification accuracy."
+                color="#fbceb1"
+                borderColor="#e0b594"  
+              />
+            </a>
+            <a
               href="https://github.com/aaru4/mediScript"
               target="_blank"
               rel="noopener noreferrer"
@@ -38,7 +51,12 @@ const projPage = () => {
             >
               <IndBox
                 title="MelaninMed"
-                org="A deep learning powered AI mobile application for racially equitable skin cancer detection."
+                org="A deep learning powered AI mobile application for racially equitable skin cancer detection. MelaninMed integrates two machine learning
+models: a malignancy classifier (ResNet-18 with an attention mechanism) and a lesion
+diagnosis system (custom ViT adaptor with a SqueezeExcite block). The malignancy classifier
+has ~99% accuracy, sensitivity, and specificity. The lesion diagnosis system diagnoses 78
+different skin conditions with 95% accuracy and sensitivity, as well as 99% specificity. There is
+no significant performance difference between skin tones"
                 color="#b1ddc9"
                 borderColor="#86b0a0"
               />
