a971e24772b7ef366615516ca14afadb78f56c65
aaru4 <98666654+aaru4@users.noreply.github.com>
2025-07-16 14:08:50 -0400

    Update page.tsx

--- app/expPage/page.tsx
+++ app/expPage/page.tsx
@@ -20,7 +20,9 @@ const ExpPage = () => {
                 title="AI Development Intern"
                 org="Georgia Institute of Technology"
                 date="JUNE 2025"
-                desc="Designing Python-based to create introspective, self-reflective LLM pipelines using DSPy to improve reasoning transparency. Advancing ethical computational creativity & AI-powered game generation.
+                desc=" Building modular LLM pipelines using Python and the DSPy framework to improve reasoning transparency and support ethical, introspective AI development.
+                Integrating visualization tools to trace LLM I/O and inspect intermediate reasoning steps for model explainability and debugging.
+                Exploring 3+ use cases in legal document review, AI-powered game generation, and IDE-assisted debugging, combining LLM introspection with user-guided workflows.
 "                color="#fbceb1"
                 borderColor="#e0b594"
               />
@@ -28,10 +30,7 @@ const ExpPage = () => {
                 title="STEM@GTRI Intern"
                 org="Georgia Tech Research Institute"
                 date="JUNE - JULY 2025"
-                desc="- Selected as the top 5% of 1200+
-- Designed and implemented an inversion-based adaptive noise cancellation system using signal generators, spectrum analyzers, and tunable band-pass filters
-- Developed a real-time digital signal processing (DSP) pipeline with statistically significant 48% average SNR improvement
-- Integrated the system into a custom-built RF transceiver for performance validation under realistic conditions
+                desc="- Selected as the top 5% of 1200+. Designed and implemented an inversion-based adaptive noise cancellation system using signal generators, spectrum analyzers, and tunable band-pass filters. Developed a real-time digital signal processing (DSP) pipeline with statistically significant 48% average SNR improvement. Integrated the system into a custom-built RF transceiver for performance validation under realistic conditions.
 "
                 color="#b1ddc9"
                 borderColor="#86b0a0"
@@ -40,7 +39,11 @@ const ExpPage = () => {
                 title="Software Development Intern"
                 org="United Nations WAY"
                 date="JAN 2025 - PRESENT"
-                desc="Selected as 1 of 1500+ as a web development intern, technical advisor, and communication manager. Organized, moderated, and judged the largest UN GNEC hackathon in history. 1700 participants, $10,000 in prizes. Attended by high-profile guests including UN Ambassadors and Directors."
+                desc="Selected as 1 of 1500+.
+                Organized, judged, and moderated the largest UN hackathon to date - 1,700+ participants, $10,000 in prizes, and attendance from UN Ambassadors and Directors.
+                Revamped WAY website for enhanced UX, accessibility, and mobile responsiveness across 50+ pages; launched novel global youth policy map with scraped data and custom fact sheets from 190+ countries.
+                Streamlined onboarding for 1,300+ volunteers; restructured content architecture for 250+ resources
+"
             color="#6cd0d0"
                 borderColor="#519e9e"
               />
@@ -48,15 +51,21 @@ const ExpPage = () => {
                 title="AI Research Intern"
                 org="Stanford Department of Anesthesiology"
                 date="JAN 2025 - PRESENT"
-                desc="Engineered predictive models and risk analysis for intraoperative hypotension and postoperative mortality from time-series EHR data matrices. Extracted hypotension-linked risk factors with SQL, R, Python. Proposing novel definitions of blood pressure lability." 
-                color="#ffcbdb"
+                desc="Engineered 50+ covariates from labs, vitals, diagnoses, and medications using SQL, Python, and BigQuery.
+                  Deployed and statistically interpreted logistic regression across 4,100+ patients.
+                  Developing a graphical modeling framework to define hypotension and blood pressure lability
+"
+                  color="#ffcbdb"
                 borderColor="#fa98b6"
               />
             <UCProjectBox
               title="Software Development Intern"
               org="Oracle"
               date="AUG 2024 - MAY 2025"
-              desc="Designed and implemented a billing system using OCI, APEX, SQL, & PL/SQL; directed FIFO debt management, payment application, and error handling, with delinquency predictions."
+              desc="Led full-stack Oracle APEX app development, from schema design to UI, for a production billing platform using Oracle Cloud Infrastructure (OCI), APEX, SQL, and PL/SQL.
+              Implemented FIFO debt resolution logic, dynamic error handling, and delinquency prediction modules.
+              Conducted independent research on multi-class ultrasound cancer detection using ResNet18 + attention, improving classification accuracy across tumor types.
+              "
               color="#fbceb1"
               borderColor="#e0b594"
             />
@@ -65,7 +74,10 @@ const ExpPage = () => {
                 title="AI Research Intern"
                 org="Stanford Center for Artificial Intelligence in Medicine and Imaging"
                 date="JUNE - JULY 2024"
-                desc="Selected as 1 of 20 nationwide. Developed high-accuracy pulmonary diagnostic system.  RadGraph for unsupervised radiology report classifier; ResNet ultrasound analysis model."
+                desc="Selected as 1 of 20 nationwide. Trained a ResNet-101 image classifier on weak labels compiled and extracted from radiology reports via rule-based NLP, SpaCy, RadGraph.
+                Built custom dataset with transformation pipelines to reduce label bias and eliminate overrepresentation.
+                Achieved 89% classification accuracy on pulmonary ultrasound data; placed 2nd in program challenge
+" 
                 color="#b1ddc9"
                 borderColor="#86b0a0"
               />
@@ -74,7 +86,10 @@ const ExpPage = () => {
               title="Software Development Intern"
               org="Microsoft"
               date="JULY 2023 - APR 2024"
-              desc="Utilized Swift, SQL, Dart, and JavaScript to develop JackTag, a mobile application and NFC wearable that streamlines communication of medical information between crisis victims and first responders. Presented to Microsoft/AT&T investors."
+              desc="Prototyped an iOS + Flutter app and NFC wearable for HIPAA-protect medical data transfer in emergencies.
+              Programmed core NFC logic in Swift, cross-platform UI with Dart, and explored offline storage with SQL.
+              Led Scrum-based management, coordinated UI/UX, and pitched to Microsoft and AT&T execs
+"
               color="#6cd0d0"
               borderColor="#519e9e"
             />
@@ -83,7 +98,8 @@ const ExpPage = () => {
                 title="AI Research Intern"
                 org="TSI + Cornell University"
                 date="JUNE - AUG 2023"
-                desc="Developed a graph neural network for binary state neuroimaging recognition. Accepted to TSI (~10% of 850), matched at Cornell University. Published paper and spoke at Talaria International Conference."
+                desc="Selected as Top 10% of ~1,000. Adapted Brain Network Transformer using Python GNNs for multi-state neuroimaging recognition.
+             Published in Talaria Journal; Speaker at Talaria 2023 International Conference."
                 color="#ffcbdb"
                 borderColor="#fa98b6"
               />
@@ -92,7 +108,10 @@ const ExpPage = () => {
               title="AI Research Intern"
               org="Midwestern University"
               date="AUG 2023 - APR 2024"
-              desc="Developed a two-prong neural network to automate gastric point-of-care ultrasound. Segments antrum, classifies content, identifies diameters, calculates operable volume, and outputs aspiration risk at 87% accuracy."
+              desc="Engineered POCUS-Net, a novel transformer-based model to automate gastric ultrasound analysis .
+              Classified gastric contents (liquid, solid, air) and segmented diameters to estimate operable volume.
+              Achieved ~87% accuracy; integrated volume-based decision weighting to output aspiration risk
+"
               color="#fbceb1"
               borderColor="#e0b594"
             />
