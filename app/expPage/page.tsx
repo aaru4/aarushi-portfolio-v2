1e818e551742606d5a66c0c465e81fdbe8db5692
aaru4 <98666654+aaru4@users.noreply.github.com>
2025-07-16 14:13:57 -0400

    Update page.tsx

--- app/expPage/page.tsx
+++ app/expPage/page.tsx
@@ -52,6 +52,8 @@ const ExpPage = () => {
                 org="Stanford Department of Anesthesiology"
                 date="JAN 2025 - PRESENT"
                 desc="Engineered 50+ covariates from labs, vitals, diagnoses, and medications using SQL, Python, and BigQuery.
+                  Extracted structured and unstructured EHR data (labs, flowsheets, medications, ICD10, vitals) to derive frailty scores, comorbidities, medication classes, and physiological predictors.
+                  Visualized variable distributions, performed imputation strategies, and constructed Table 1 stratified by blood pressure categories with standardized mean differences.
                   Deployed and statistically interpreted logistic regression across 4,100+ patients.
                   Developing a graphical modeling framework to define hypotension and blood pressure lability
 "
