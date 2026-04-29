e89a237d560ff8f9ff96ff8fed12fab7a218af12
aaru4 <98666654+aaru4@users.noreply.github.com>
2026-04-29 15:04:54 -0400

    Update page.tsx

--- app/eduPage/page.tsx
+++ app/eduPage/page.tsx
@@ -12,7 +12,7 @@ const eduPage = () => {
 
       <div className="flex items-center justify-center w-full">
         <div id="education" className="bg-white p-3 rounded-lg">
-          <h1 className="text-4xl font-bold text-center underline text-black">
+          <h1 className="text-3xl font-bold text-center underline text-black">
             Education & Skills
           </h1>
         </div>
